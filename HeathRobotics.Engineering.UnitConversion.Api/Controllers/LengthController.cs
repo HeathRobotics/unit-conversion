5233c4b9c4a688e1a73d13092606f3603dc66fa1
Todd Heath <toddaheath@gmail.com>
2020-08-26 14:12:43 -0400

    check for required values.

--- HeathRobotics.Engineering.UnitConversion.Api/Controllers/LengthController.cs
+++ HeathRobotics.Engineering.UnitConversion.Api/Controllers/LengthController.cs
@@ -6,12 +6,13 @@ using HeathRobotics.Engineering.UnitConversion.Api.Models.Length;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AutoMapper;
+using Microsoft.AspNetCore.Mvc.Core;
 
 namespace HeathRobotics.Engineering.UnitConversion.Api.Controllers
 {
     [ApiController]
     [Route("api/unit_conversion/length")]
-    public class LengthController
+    public class LengthController : ControllerBase
     {
         private readonly ILogger<LengthController> logger;
         private readonly IAggregateUnitConversionService unitConversionService;
@@ -24,14 +25,14 @@ namespace HeathRobotics.Engineering.UnitConversion.Api.Controllers
 
             #region mapper configuration
             var mapperConfiguration = new MapperConfiguration(
-                cfg => 
+                cfg =>
                 {
                     cfg.CreateMap<LengthMeasureConversionModel, LengthMeasure>()
                     .ForMember(dest => dest.Prefix, opt => opt.MapFrom(src => Enum.Parse(typeof(PrefixUnits), src.Prefix, true)))
                     .ForMember(dest => dest.Units, opt => opt.MapFrom(src => Enum.Parse(typeof(LengthUnits), src.Units, true)));
                     cfg.CreateMap<LengthMeasure, LengthMeasureConversionModel>()
-                    .ForMember(dest => dest.Prefix, opt => opt.MapFrom(src => src.Prefix.ToString()))
-                    .ForMember(dest => dest.Units, opt => opt.MapFrom(src => src.Units.ToString()));
+                    .ForMember(dest => dest.Prefix, opt => opt.MapFrom(src => src.Prefix.ToString().ToLower()))
+                    .ForMember(dest => dest.Units, opt => opt.MapFrom(src => src.Units.ToString().ToLower()));
 
                 });
             this.mapper = new Mapper(mapperConfiguration);
@@ -42,6 +43,16 @@ namespace HeathRobotics.Engineering.UnitConversion.Api.Controllers
         public ActionResult<LengthMeasureConversionModel> Convert([FromBody] LengthMeasureConversionModel apiModel)
         {
             var dslModel = this.mapper.Map<LengthMeasureConversionModel, LengthMeasure>(apiModel);
+
+            if (string.IsNullOrWhiteSpace(apiModel.TargetPrefix))
+            {
+                return BadRequest("A target prefix is required.");
+            }
+
+            if (string.IsNullOrWhiteSpace(apiModel.TargetUnits))
+            {
+                return BadRequest("A target prefix is required.");
+            }
 
             var targetPrefix = (PrefixUnits)Enum.Parse(typeof(PrefixUnits), apiModel.TargetPrefix, true);
             var targetUnits = (LengthUnits)Enum.Parse(typeof(LengthUnits), apiModel.TargetUnits, true);
