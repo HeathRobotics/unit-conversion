becb49c88cf24b0c406926860698370908a8772f
Todd Heath <toddaheath@gmail.com>
2020-09-03 14:55:23 -0400

    Added to string test.

---
+++ Tests/HeathRobotics.Engineering.UnitConversion.Tests/LengthMeasureTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HeathRobotics.Engineering.UnitConversion.Tests
+{
+    public class LengthMeasureTests
+    {
+        public LengthMeasureTests()
+        {
+
+        }
+
+        [Theory]
+        [InlineData(5.0, PrefixUnits.None, LengthUnits.Feet, "5 ft")]
+        public void ToStringTest(double value, PrefixUnits prefix, LengthUnits units, string expectedValue)
+        {
+            var measure = new LengthMeasure()
+            {
+                Value = value,
+                Prefix = prefix,
+                Units = units
+            };
+
+            var result = measure.ToString();
+            Assert.True(!string.IsNullOrWhiteSpace(result));
+            Assert.Equal(expectedValue, result);
+
+        }
+        
+    }
+}
